7deaaf604f1ead71bb6ebc3c3367e944822ebc14
Chris Lahaye <mail@chrislahaye.com>
2021-10-19 11:55:19 +0200

    feat(LemonadeMarketplace): add deploy script
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

---
+++ deploy/01_LemonadeMarketplace.ts
@@ -0,0 +1,16 @@
+import { DeployFunction } from 'hardhat-deploy/types';
+import { ethers } from 'hardhat';
+
+const FEE_MAKER = '0x951292004e8a18955Cb1095CB72Ca6B01d68336E';
+const FEE_RATIO = ethers.utils.parseEther('0.05');
+
+const deployFunction: DeployFunction = async function ({ deployments: { deploy }, getUnnamedAccounts }) {
+  const [from] = await getUnnamedAccounts();
+
+  await deploy('LemonadeMarketplace', {
+    args: [FEE_MAKER, FEE_RATIO],
+    from,
+  });
+};
+
+export default deployFunction;
