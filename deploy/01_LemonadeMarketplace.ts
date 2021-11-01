835083ff4c809d940681381119ad04e1f867df10
Chris Lahaye <mail@chrislahaye.com>
2021-11-01 13:34:19 +0100

    feat(LemonadeMarketplace): set fee percentage to 2%
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- deploy/01_LemonadeMarketplace.ts
+++ deploy/01_LemonadeMarketplace.ts
@@ -2,7 +2,7 @@ import { DeployFunction } from 'hardhat-deploy/types';
 import { ethers } from 'hardhat';
 
 const FEE_MAKER = '0x951292004e8a18955Cb1095CB72Ca6B01d68336E';
-const FEE_RATIO = ethers.utils.parseEther('0.05');
+const FEE_RATIO = ethers.utils.parseEther('0.02');
 
 const deployFunction: DeployFunction = async function ({ deployments: { deploy }, getUnnamedAccounts }) {
   const [from] = await getUnnamedAccounts();
