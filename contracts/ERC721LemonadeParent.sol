2a7b5e29c63c3d527f03628bd06ca099d100c28b
Chris Lahaye <mail@chrislahaye.com>
2021-11-28 12:54:10 +0100

    feat(ERC721LemonadeParent)!: add extended royalties support
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- contracts/ERC721LemonadeParent.sol
+++ contracts/ERC721LemonadeParent.sol
@@ -2,15 +2,14 @@
 
 pragma solidity ^0.8.0;
 
+import "./Royalties.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
-import "./ERC721Royalty.sol";
 
-contract ERC721LemonadeParent is Context, AccessControlEnumerable, ERC721Enumerable, ERC721Burnable, ERC721Pausable, ERC721Royalty {
+contract ERC721LemonadeParent is Context, AccessControlEnumerable, ERC721Burnable, ERC721Pausable, Royalties {
     bytes32 public constant PREDICATE_ROLE = keccak256("PREDICATE_ROLE");
 
     mapping (uint256 => string) private _tokenURIs;
@@ -36,7 +35,7 @@ contract ERC721LemonadeParent is Context, AccessControlEnumerable, ERC721Enumera
         public
         view
         virtual
-        override(AccessControlEnumerable, ERC721, ERC721Enumerable, ERC721Royalty)
+        override(AccessControlEnumerable, ERC721, Royalties)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
@@ -45,7 +44,7 @@ contract ERC721LemonadeParent is Context, AccessControlEnumerable, ERC721Enumera
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
         virtual
-        override(ERC721, ERC721Enumerable, ERC721Pausable)
+        override(ERC721, ERC721Pausable)
     {
         super._beforeTokenTransfer(from, to, tokenId);
     }
@@ -61,26 +60,6 @@ contract ERC721LemonadeParent is Context, AccessControlEnumerable, ERC721Enumera
     }
 
     /**
-     * @dev see https://github.com/maticnetwork/pos-portal/blob/88dbf0a88fd68fa11f7a3b9d36629930f6b93a05/flat/DummyMintableERC721.sol#L2181
-     */
-    function writeState(uint256 tokenId, bytes memory data)
-        internal
-        virtual
-    {
-        (
-            string memory tokenURI_,
-            address royaltyMaker,
-            uint256 royaltyFraction
-        ) = abi.decode(data, (string, address, uint256));
-
-        _tokenURIs[tokenId] = tokenURI_;
-
-        if (royaltyMaker != address(0)) {
-            _setRoyalty(tokenId, royaltyMaker, royaltyFraction);
-        }
-    }
-
-    /**
      * @dev see https://github.com/maticnetwork/pos-portal/blob/88dbf0a88fd68fa11f7a3b9d36629930f6b93a05/flat/DummyMintableERC721.sol#L2199
      */
     function mint(address to, uint256 tokenId, bytes calldata data)
@@ -89,7 +68,7 @@ contract ERC721LemonadeParent is Context, AccessControlEnumerable, ERC721Enumera
     {
         _mint(to, tokenId);
 
-        writeState(tokenId, data);
+        _saveState(tokenId, data);
     }
 
     /**
@@ -101,5 +80,19 @@ contract ERC721LemonadeParent is Context, AccessControlEnumerable, ERC721Enumera
         returns (bool)
     {
         return _exists(tokenId);
+    }
+
+   /**
+     * @dev see https://github.com/maticnetwork/pos-portal/blob/88dbf0a88fd68fa11f7a3b9d36629930f6b93a05/flat/DummyMintableERC721.sol#L2181
+     */
+    function _saveState(uint256 tokenId, bytes memory data)
+        internal
+        virtual
+    {
+        (string memory tokenURI_, LibPart.Part[] memory royalties) = abi.decode(data, (string, LibPart.Part[]));
+
+        _tokenURIs[tokenId] = tokenURI_;
+
+        _saveRoyalties(tokenId, royalties);
     }
 }
