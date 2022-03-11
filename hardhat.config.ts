c6282c08bd6158d8dee46b41586cb6dfaf3623c0
Chris Lahaye <mail@chrislahaye.com>
2022-03-11 10:29:32 +0100

    feat(misc): enable optimizer
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -51,7 +51,14 @@ const config: HardhatUserConfig = {
       deploy: ['deploy/polygon'],
     },
   },
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
 };
 
 export default config;
