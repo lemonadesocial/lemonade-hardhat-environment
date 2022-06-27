d182bc8ae172a4673326a7dc361c23157a28a547
Chris Lahaye <mail@chrislahaye.com>
2022-06-27 14:20:58 +0530

    style(LemonadeMarketplace): format contracts
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- contracts/LemonadeMarketplaceV1Forwardable.sol
+++ contracts/LemonadeMarketplaceV1Forwardable.sol
@@ -5,7 +5,10 @@ pragma solidity ^0.8.0;
 import "./LemonadeMarketplaceV1.sol";
 import "./RelayRecipient.sol";
 
-contract LemonadeMarketplaceV1Forwardable is LemonadeMarketplaceV1, RelayRecipient {
+contract LemonadeMarketplaceV1Forwardable is
+    LemonadeMarketplaceV1,
+    RelayRecipient
+{
     constructor(
         address feeAccount,
         uint96 feeValue,
