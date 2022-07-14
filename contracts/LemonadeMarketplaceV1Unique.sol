a813fd02b0e9bc46af1565300dc933449adb2516
Chris Lahaye <mail@chrislahaye.com>
2022-07-14 11:22:43 +0200

    feat(LemonadeMarketplaceV1Unique): add Unique variant of Lemonade marketplace contract
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

---
+++ contracts/LemonadeMarketplaceV1Unique.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./LemonadeMarketplaceV1.sol";
+import "./unique/ICollection.sol";
+
+contract LemonadeMarketplaceV1Unique is LemonadeMarketplaceV1 {
+    constructor(address feeAccount, uint96 feeValue)
+        LemonadeMarketplaceV1(feeAccount, feeValue)
+    {}
+
+    function getRaribleV2Royalties(address tokenContract, uint256 tokenId)
+        public
+        view
+        override
+        returns (bool, LibPart.Part[] memory)
+    {
+        if (!isCollection(tokenContract)) {
+            return
+                LemonadeMarketplaceV1.getRaribleV2Royalties(
+                    tokenContract,
+                    tokenId
+                );
+        }
+
+        try
+            ICollection(tokenContract).property(tokenId, ROYALTIES_PROPERTY)
+        returns (bytes memory data) {
+            LibPart.Part[] memory royalties = abi.decode(
+                data,
+                (LibPart.Part[])
+            );
+            return (true, royalties);
+        } catch {
+            return (false, new LibPart.Part[](0));
+        }
+    }
+
+    function isCollection(address addr) private pure returns (bool) {
+        return
+            uint160(addr) & 0x00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000 ==
+            0x0017c4e6453cc49aaaaeaca894e6d9683e00000000;
+    }
+}
