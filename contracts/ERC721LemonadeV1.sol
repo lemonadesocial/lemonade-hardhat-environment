d0b3bcf88857caf669355b944ad1a919c44145c6
Chris Lahaye <mail@chrislahaye.com>
2022-08-08 12:25:51 +0200

    feat(ERC721LemonadeV1): refactor royalties implementation
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- contracts/ERC721LemonadeV1.sol
+++ contracts/ERC721LemonadeV1.sol
@@ -2,10 +2,13 @@
 
 pragma solidity ^0.8.0;
 
-import "./Royalties.sol";
+import "./rarible/LibPart.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+
+bytes4 constant ERC2981_INTERFACE_ID = 0x2a55205a;
+bytes4 constant RaribleRoyaltiesV2_INTERFACE_ID = 0xcad96cca;
 
 interface IMintable {
     function mintToCaller(string memory tokenURI) external returns (uint256);
@@ -16,12 +19,13 @@ interface IMintable {
     ) external returns (uint256);
 }
 
-contract ERC721LemonadeV1 is ERC721, Royalties, Ownable, IMintable {
+contract ERC721LemonadeV1 is ERC721, Ownable, IMintable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdTracker;
 
     mapping(uint256 => string) private _tokenURIs;
+    mapping(uint256 => LibPart.Part[]) private _royalties;
 
     constructor(string memory name, string memory symbol)
         ERC721(name, symbol)
@@ -44,11 +48,17 @@ contract ERC721LemonadeV1 is ERC721, Royalties, Ownable, IMintable {
 
     function mintToCallerWithRoyalty(
         string memory tokenURI_,
-        LibPart.Part[] memory royalties_
+        LibPart.Part[] memory royalties
     ) public override returns (uint256) {
         uint256 tokenId = mintToCaller(tokenURI_);
 
-        _saveRoyalties(tokenId, royalties_);
+        uint256 length = royalties.length;
+        for (uint256 i; i < length; ) {
+            _royalties[tokenId].push(royalties[i]);
+            unchecked {
+                ++i;
+            }
+        }
 
         return tokenId;
     }
@@ -72,9 +82,38 @@ contract ERC721LemonadeV1 is ERC721, Royalties, Ownable, IMintable {
         public
         view
         virtual
-        override(ERC721, Royalties)
+        override
         returns (bool)
     {
-        return super.supportsInterface(interfaceId);
+        return
+            interfaceId == ERC2981_INTERFACE_ID ||
+            interfaceId == RaribleRoyaltiesV2_INTERFACE_ID ||
+            super.supportsInterface(interfaceId);
+    }
+
+    function getRaribleV2Royalties(uint256 tokenId)
+        public
+        view
+        returns (LibPart.Part[] memory)
+    {
+        return _royalties[tokenId];
+    }
+
+    function royaltyInfo(uint256 tokenId, uint256 price)
+        public
+        view
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        uint256 length = _royalties[tokenId].length;
+
+        if (length == 0) {
+            return (address(0), 0);
+        }
+
+        uint256 totalValue;
+        for (uint256 i; i < length; i++) {
+            totalValue += _royalties[tokenId][i].value;
+        }
+        return (_royalties[tokenId][0].account, (price * totalValue) / 10000);
     }
 }
