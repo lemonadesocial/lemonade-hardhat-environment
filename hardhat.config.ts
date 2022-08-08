f0d6ef9b60a9b103b4be8c1c8b11e764035ac01b
Chris Lahaye <mail@chrislahaye.com>
2022-08-08 11:18:24 +0200

    feat: add rpc url environment variable overwrites
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -21,73 +21,73 @@ const config: HardhatUserConfig = {
   },
   networks: {
     'aurora': {
-      url: 'https://mainnet.aurora.dev/',
+      url: process.env.NETWORK_AURORA_URL || 'https://mainnet.aurora.dev/',
       chainId: 1313161554,
       accounts,
       deploy: ['deploy/__mainnet__', 'deploy/aurora'],
     },
     'aurora-testnet': {
-      url: 'https://testnet.aurora.dev/',
+      url: process.env.NETWORK_AURORA_TESTNET_URL || 'https://testnet.aurora.dev/',
       chainId: 1313161555,
       accounts,
       deploy: ['deploy/__testnet__', 'deploy/aurora-testnet'],
     },
     'bnb': {
-      url: 'https://bsc-dataseed.binance.org/',
+      url: process.env.NETWORK_BNB_URL || 'https://bsc-dataseed.binance.org/',
       chainId: 56,
       accounts,
       deploy: ['deploy/__mainnet__', 'deploy/bnb'],
     },
     'bnb-testnet': {
-      url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+      url: process.env.NETWORK_BNB_TESTNET_URL || 'https://data-seed-prebsc-1-s1.binance.org:8545/',
       chainId: 97,
       accounts,
       deploy: ['deploy/__testnet__', 'deploy/bnb-testnet'],
     },
-    development: {
+    'development': {
       url: 'http://127.0.0.1:8545/',
       chainId: 31337,
       accounts,
       deploy: ['deploy/development'],
     },
-    ethereum: {
-      url: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    'ethereum': {
+      url: process.env.NETWORK_ETHEREUM_URL || 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
       chainId: 1,
       accounts,
       deploy: ['deploy/ethereum'],
     },
-    mumbai: {
-      url: 'https://rpc-mumbai.matic.today',
+    'mumbai': {
+      url: process.env.NETWORK_MUMBAI_URL || 'https://rpc-mumbai.matic.today',
       chainId: 80001,
       accounts,
       deploy: ['deploy/__testnet__', 'deploy/mumbai'],
     },
-    opal: {
-      url: 'https://rpc-opal.unique.network/',
+    'opal': {
+      url: process.env.NETWORK_OPAL_URL || 'https://rpc-opal.unique.network/',
       chainId: 8882,
       accounts,
       deploy: ['deploy/__testnet__', 'deploy/opal'],
     },
-    polygon: {
-      url: 'https://polygon-rpc.com/',
+    'polygon': {
+      url: process.env.NETWORK_POLYGON_URL || 'https://polygon-rpc.com/',
       chainId: 137,
       accounts,
       deploy: ['deploy/__mainnet__', 'deploy/polygon'],
     },
-    goerli: {
-      url: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    'goerli': {
+      url: process.env.NETWORK_GOERLI_URL || 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
       chainId: 5,
       accounts,
       deploy: ['deploy/goerli'],
     },
-    moonbase: {
-      url: 'https://moonbeam-alpha.api.onfinality.io/public',
+    'moonbase': {
+      url: process.env.NETWORK_MOONBASE_URL || 'https://moonbeam-alpha.api.onfinality.io/public',
       chainId: 1287,
       accounts,
       deploy: ['deploy/__testnet__', 'deploy/moonbase'],
     },
-    moonbeam: {
-      url: 'https://moonbeam.api.onfinality.io/public',
+    'moonbeam': {
+      url: process.env.NETWORK_MOONBEAM_URL || 'https://moonbeam.api.onfinality.io/public',
       chainId: 1284,
       accounts,
       deploy: ['deploy/__mainnet__', 'deploy/moonbeam'],
