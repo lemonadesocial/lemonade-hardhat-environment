c5cd4a19f236f7f49daa7b08aacb1a2c1eaa78a7
Chris Lahaye <mail@chrislahaye.com>
2022-08-11 11:29:02 +0200

    feat: deploy non-ethereum contract on goerli
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- deploy/goerli/ERC721LemonadeV1.ts
+++ deploy/goerli/ERC721LemonadeV1.ts
@@ -2,13 +2,12 @@ import type { DeployFunction } from 'hardhat-deploy/types';
 
 const ERC721_NAME = 'Non-Fungible Lemon';
 const ERC721_SYMBOL = 'NFL';
-const PROXY_REGISTRY_ADDRESS = '0x1E525EEAF261cA41b809884CBDE9DD9E1619573A';
 
 const deployFunction: DeployFunction = async function ({ deployments: { deploy }, getNamedAccounts }) {
   const { deployer: from } = await getNamedAccounts();
 
-  await deploy('ERC721LemonadeV1Ethereum', {
-    args: [ERC721_NAME, ERC721_SYMBOL, PROXY_REGISTRY_ADDRESS],
+  await deploy('ERC721LemonadeV1', {
+    args: [ERC721_NAME, ERC721_SYMBOL],
     from,
     log: true,
   });
