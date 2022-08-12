49acd6c7f276944e31e950d560628644bcae37c3
Chris Lahaye <mail@chrislahaye.com>
2022-08-12 20:50:58 +0200

    feat(LemonadePoapV1ChainlinkRequestForwardable): add variant of lemonade poap contract
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

---
+++ contracts/LemonadePoapV1ChainlinkRequestForwardable.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./LemonadePoapV1ChainlinkRequest.sol";
+import "./RelayRecipient.sol";
+
+contract LemonadePoapV1ChainlinkRequestForwardable is
+    LemonadePoapV1ChainlinkRequest,
+    RelayRecipient
+{
+    constructor(
+        string memory name,
+        string memory symbol,
+        address creator,
+        string memory tokenURI,
+        LibPart.Part[] memory royalties,
+        uint256 totalSupply,
+        address accessRegistry,
+        address chainlinkRequest,
+        address trustedForwarder
+    )
+        LemonadePoapV1ChainlinkRequest(
+            name,
+            symbol,
+            creator,
+            tokenURI,
+            royalties,
+            totalSupply,
+            accessRegistry,
+            chainlinkRequest
+        )
+        RelayRecipient(trustedForwarder)
+    {}
+
+    function _msgSender()
+        internal
+        view
+        virtual
+        override(Context, RelayRecipient)
+        returns (address)
+    {
+        return RelayRecipient._msgSender();
+    }
+}
