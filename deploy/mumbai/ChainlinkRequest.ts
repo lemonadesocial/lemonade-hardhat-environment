99d2cc38eea96dd806eeb78c190b318c26bc09aa
Chris Lahaye <mail@chrislahaye.com>
2022-08-20 13:21:26 +0200

    feat: add operator mumbai deployment
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- deploy/mumbai/ChainlinkRequest.ts
+++ deploy/mumbai/ChainlinkRequest.ts
@@ -4,8 +4,8 @@ import { deployFunction } from '../../services/ChainlinkRequest';
 
 export default deployFunction({
   chainlinkToken: '0x326c977e6efc84e512bb9c30f76e30c160ed06fb',
-  chainlinkOracle: '0xB51Dc0a6d7a532F7FE8909D631875b13999eE8d1',
-  jobId: '6b9f2de7cc3a4d4b8e98ab63fc8e9026',
-  fee: ethers.utils.parseEther('0.01'),
+  chainlinkOracle: '0x58BdCe3F5f05F51f4B6ceB79aC0d150aed2D5a14',
+  jobId: '02ec7f2539534de3b94fac17dbfc8d20',
+  fee: ethers.utils.parseEther('0.001'),
   url: 'https://backend.staging.lemonade.social/chainlink?network=mumbai',
 });
---
+++ deploy/mumbai/Operator.ts
@@ -0,0 +1,6 @@
+import { deployFunction } from '../../services/Operator';
+
+export default deployFunction({
+  authorizedSenders: ['0x717703c58085D6ab9B9144aD6F7fe19A6Fb72fEe'],
+  chainlinkToken: '0x326c977e6efc84e512bb9c30f76e30c160ed06fb'
+});
