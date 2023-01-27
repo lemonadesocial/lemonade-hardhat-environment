0533c079208bd5e3f90f83b5e0c74d2b23c3c4d9
Chris Lahaye <mail@chrislahaye.com>
2023-01-27 17:45:49 +0100

    feat(AccessRegistry): add arbitrum one festival heads operator
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- deploy/arbitrum-one/AccessRegistry.ts
+++ deploy/arbitrum-one/AccessRegistry.ts
@@ -1,7 +1,7 @@
 import { deployFunction } from "../../services/AccessRegistry";
 
 export default deployFunction([
-  { role: 'TRUSTED_CLAIMER_ROLE',   account: '0xB25D3d684EAB618B51eE8A3127FBacA6224DaFA9',  grant: true },
-  { role: 'TRUSTED_OPERATOR_ROLE',  account: '0xB25D3d684EAB618B51eE8A3127FBacA6224DaFA9',  grant: true },
+  { role: 'TRUSTED_CLAIMER_ROLE',         account: '0xB25D3d684EAB618B51eE8A3127FBacA6224DaFA9',  grant: true },
+  { role: 'TRUSTED_OPERATOR_ROLE',        account: '0xB25D3d684EAB618B51eE8A3127FBacA6224DaFA9',  grant: true },
+  { role: 'FESTIVAL_HEADS_OPERATOR_ROLE', account: '0xB32bF33D8Bf52d9f526CdF64EDD2f337Ac23f8C1',  grant: true },
 ]);
-
