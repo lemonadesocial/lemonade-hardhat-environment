53ff370b1b336a966e0e28ebdb3e5452356a6d23
Chris Lahaye <mail@chrislahaye.com>
2023-05-04 19:07:11 +0200

    feat: remove sapphire chain
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- deploy/sapphire/AccessRegistry.ts
+++
@@ -1,6 +0,0 @@
-import { deployFunction } from "../../services/AccessRegistry";
-
-export default deployFunction([
-  { role: 'TRUSTED_CLAIMER_ROLE',   account: '0xB25D3d684EAB618B51eE8A3127FBacA6224DaFA9',  grant: true },
-  { role: 'TRUSTED_OPERATOR_ROLE',  account: '0xB25D3d684EAB618B51eE8A3127FBacA6224DaFA9',  grant: true },
-]);
