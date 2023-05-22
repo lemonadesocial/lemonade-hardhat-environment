f862298bf6800ae5219028d754418621fa5db2ea
Chris Lahaye <mail@chrislahaye.com>
2023-05-22 20:59:11 +0200

    feat: add mumbai and polygon festival heads operator
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- deploy/mumbai/AccessRegistry.ts
+++ deploy/mumbai/AccessRegistry.ts
@@ -1,6 +1,7 @@
 import { deployFunction } from "../../services/AccessRegistry";
 
 export default deployFunction([
-  { role: 'TRUSTED_CLAIMER_ROLE',   account: '0x69aFB95996970D07eFBCb46Bdc16223fac18FB84',  grant: true },
-  { role: 'TRUSTED_OPERATOR_ROLE',  account: '0x69aFB95996970D07eFBCb46Bdc16223fac18FB84',  grant: true },
+  { role: 'TRUSTED_CLAIMER_ROLE',         account: '0x69aFB95996970D07eFBCb46Bdc16223fac18FB84',  grant: true },
+  { role: 'TRUSTED_OPERATOR_ROLE',        account: '0x69aFB95996970D07eFBCb46Bdc16223fac18FB84',  grant: true },
+  { role: 'FESTIVAL_HEADS_OPERATOR_ROLE', account: '0xfb756b44060e426731e54e9f433c43c75ee90d9f',  grant: true },
 ]);
