f7a4510e98be5ecbc1da649106c311fef1e76393
Chris Lahaye <mail@chrislahaye.com>
2024-02-01 17:01:20 +0100

    fix(passport): fix deploy base script
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- scripts/deploy-base.ts
+++ scripts/deploy-base.ts
@@ -1,6 +1,6 @@
 import { ethers, upgrades } from 'hardhat';
 
-const { AXELAR_GATEWAY, AXELAR_GAS_SERVICE, AXELAR_NETWORKS, CALL_ADDRESS, MAX_SUPPLY } = process.env;
+const { AXELAR_GATEWAY, AXELAR_GAS_SERVICE, AXELAR_NETWORKS, CALL_ADDRESS, CALL_NETWORK, MAX_SUPPLY } = process.env;
 
 async function main() {
   const contractFactory = await ethers.getContractFactory('BaseV1');
@@ -10,6 +10,7 @@ async function main() {
     AXELAR_GAS_SERVICE,
     AXELAR_NETWORKS && JSON.parse(AXELAR_NETWORKS) || [],
     CALL_ADDRESS || ethers.constants.AddressZero,
+    CALL_NETWORK && ethers.utils.keccak256(ethers.utils.toUtf8Bytes(CALL_NETWORK)),
     MAX_SUPPLY,
   ]);
 
