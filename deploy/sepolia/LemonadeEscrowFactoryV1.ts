923561e7e393f40e1cda1d84347bd8494a5971db
Thong Dang <thong.dang@coderpush.com>
2024-02-06 20:09:02 +0700

    feat(payment-escrow): update deployment scripts

--- deploy/sepolia/LemonadeEscrowFactoryV1.ts
+++ deploy/sepolia/LemonadeEscrowFactoryV1.ts
@@ -1,10 +1,12 @@
 import type { DeployFunction } from 'hardhat-deploy/types';
 
+const INITIAL_SIGNER = '0xca5AD04d2b42985134BB2E85E78CA6f143787e3B';
+
 const deployFunction: DeployFunction = async function ({ deployments: { deploy }, getNamedAccounts }) {
   const { deployer: from } = await getNamedAccounts();
 
   await deploy('LemonadeEscrowFactoryV1', {
-    args: [],
+    args: [INITIAL_SIGNER],
     from,
     log: true,
   });
