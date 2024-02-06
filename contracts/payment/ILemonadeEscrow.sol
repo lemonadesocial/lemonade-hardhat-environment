3b908b441d83ef677768050ffd00cec0948aa9b5
Thong Dang <thong.dang@coderpush.com>
2024-02-06 20:09:02 +0700

    feat(payment): add escrow contracts

---
+++ contracts/payment/ILemonadeEscrow.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+struct RefundPolicy {
+    uint32 timestamp;
+    uint16 percent;
+}
+
+struct Deposit {
+    address token;
+    uint256 amount;
+}
+
+interface ILemonadeEscrow {
+    /**
+     * Deposit an amount of ERC20 / native token to the payment.
+     * Incase of ERC20 deposit, guest have to approve the allowance to the contract before calling this.
+     * @param paymentId id of the payment to deposit
+     * @param token contract address of token, zero address for native token
+     * @param depositAmount the amount to deposit, if native token then this must match msg.value
+     */
+    function deposit(
+        uint256 paymentId,
+        address token,
+        uint256 depositAmount
+    ) external payable;
+
+    /**
+     * Return the all deposits of a guest for a specific payment
+     * @param paymentId id of the payment
+     * @param guest wallet address of guest
+     */
+    function getDeposits(
+        uint256 paymentId,
+        address guest
+    ) external view returns (Deposit[] memory);
+
+    /**
+     * Cancel and refund the deposit amount to caller, both ERC20 and native token if any.
+     * Refund amount affected by policies. If escrow is closed then must call claimRefund instead.
+     * @param paymentId id of the payment
+     */
+    function cancelByGuest(uint256 paymentId) external;
+
+    /**
+     * Host cancel all the payments. Guests will have to call refund manually.
+     */
+    function closeEscrow() external;
+
+    /**
+     * Host cancel a specific payment and allow user to claim refund.
+     * @param paymentId id of the payment to cancel
+     */
+    function cancel(uint256 paymentId) external;
+
+    /**
+     * Claim the deposit amount to called, both ERC20 and native token if any.
+     * This can only be called after host has cancel the payment.
+     * @param paymentId id of the payment to claim
+     */
+    function claimRefund(uint256 paymentId) external;
+
+    /**
+     * Check if the caller can claim refund for the payment.
+     * @param paymentId id of the payment to check
+     */
+    function canClaimRefund(uint256 paymentId) external view returns (bool);
+}
