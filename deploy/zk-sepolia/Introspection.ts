e13ef924f34292d4964f9072b3b34a17cfbb758a
thongxuan <thong.dang@coderpush.com>
2024-07-04 16:44:40 +0700

    feat(relay-payment): deploy relay payment to zk-sepolia & zk-link-nova (#48)

--- deploy/zk-sepolia/Introspection.ts
+++
@@ -1,7 +0,0 @@
-import type { DeployFunction } from 'hardhat-deploy/types';
-
-const deployFunction: DeployFunction = async function ({ deployer }) {
-  await deployer.deploy('Introspection');
-};
-
-export default deployFunction;
