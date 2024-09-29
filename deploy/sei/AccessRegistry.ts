5126b419c3cf17645caa2b5a4f1a270455ea9e67
Thong Dang <thong.dang@coderpush.com>
2024-09-29 12:45:55 +0700

    feat(sei): re-deploy access registry & payment contracts

--- deploy/sei/AccessRegistry.ts
+++ deploy/sei/AccessRegistry.ts
@@ -3,5 +3,5 @@ import { deployFunction } from "../../services/AccessRegistry";
 export default deployFunction([
   { role: 'TRUSTED_CLAIMER_ROLE',   account: '0xB25D3d684EAB618B51eE8A3127FBacA6224DaFA9',  grant: true },
   { role: 'TRUSTED_OPERATOR_ROLE',  account: '0xB25D3d684EAB618B51eE8A3127FBacA6224DaFA9',  grant: true },
-  { role: 'PAYMENT_ADMIN_ROLE',     account: '0x6950Af4b7815c15073066C7540b1c64Fa7Be97F1',  grant: true },
+  { role: 'PAYMENT_ADMIN_ROLE',     account: '0x951292004e8a18955Cb1095CB72Ca6B01d68336E',  grant: true },
 ]);
