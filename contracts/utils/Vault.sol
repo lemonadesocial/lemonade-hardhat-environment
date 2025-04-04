426fadebf3bc90274c300bfc32ce53ce5a6bae4c
thongxuan <thong.dang@coderpush.com>
2025-04-04 11:48:36 +0700

    feat(donation): implement donation & donation registry contracts (#63)
    https://trello.com/c/8IUBd6A1/1038-donation-feature

--- contracts/utils/Vault.sol
+++ contracts/utils/Vault.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
 import "./Transferable.sol";
