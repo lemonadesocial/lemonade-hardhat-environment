e0fccce068cfe41c4c97cc06dacda95243494f61
Thong Dang <thong.dang@coderpush.com>
2025-05-07 15:52:57 +0700

    feat: redeploy lens testnet contracts

--- deploy/lens-testnet/AccessRegistry.ts
+++ deploy/lens-testnet/AccessRegistry.ts
@@ -1,6 +1,6 @@
-import { deployFunction } from "../../services/AccessRegistry";
+import { deployZkFunction } from "../../services/AccessRegistry";
 
-export default deployFunction([
+export default deployZkFunction([
   { role: 'TRUSTED_CLAIMER_ROLE',   account: '0x69aFB95996970D07eFBCb46Bdc16223fac18FB84',  grant: true },
   { role: 'TRUSTED_OPERATOR_ROLE',  account: '0x69aFB95996970D07eFBCb46Bdc16223fac18FB84',  grant: true },
   { role: 'PAYMENT_ADMIN_ROLE',     account: '0xf4a5990Ab778A2c760Be43f3380725BFC4D78b14',  grant: true },
